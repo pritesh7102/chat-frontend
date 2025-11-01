cd902eff7b94c9efc6f88bd3d4352d186f865038
Pritesh <pritesh7102@gmail.com>
2025-11-01 10:25:15 +0530

    changed backend URL

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://chat-backend-n62b.onrender.com/api",
+  // baseURL: "https://chat-backend-n62b.onrender.com/api",
   withCredentials: true,
 });
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,11 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+// const BASE_URL =
+import.meta.env.MODE === "development"
+  ? "http://localhost:5001"
+  : "https://chat-backend-n62b.onrender.com//";
+// const BASE_URL = "https://chat-backend-n62b.onrender.com/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
